32430b61fab3f6bec7fbbfdd4919fc86f404ed46
PrecisionApps <73882757+PrecisionApps@users.noreply.github.com>
2021-12-25 18:30:50 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         .target(
             name: "WordNet",
             dependencies: ["MorphologicalAnalysis"],
-            resources: [.process("turkish_wordnet.xml"),.process("english_wordnet_version_31.xml"),.process("english_exception.xml")]),
+            resources: [.process("turkish_wordnet.xml"),.process("turkish1944_wordnet.xml"),.process("turkish1955_wordnet.xml"),.process("turkish1959_wordnet.xml"),.process("turkish1966_wordnet.xml"),.process("turkish1969_wordnet.xml"),.process("turkish1974_wordnet.xml"),.process("english_wordnet_version_31.xml"),.process("english_exception.xml")]),
         .testTarget(
             name: "WordNetTests",
             dependencies: ["WordNet"]),
